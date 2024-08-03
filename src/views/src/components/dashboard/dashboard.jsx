72cc607771ba699bb3d439d4b42444c7b79ac3ca
AlanaK2 <alanakarine27@gmail.com>
2024-08-03 18:57:11 -0300

    tela login concertada, dashboard pegando dados do back

--- src/views/src/components/dashboard/dashboard.jsx
+++ src/views/src/components/dashboard/dashboard.jsx
@@ -1,39 +1,73 @@
-import React from 'react';
-import './dashboard.css';
-import Sidebar from '../sidebar/sidebarNoted';
+import React, { useState, useEffect } from "react";
+import "./dashboard.css";
+import Sidebar from "../sidebar/sidebarNoted";
+import useRootClass from "../../useRootClass";
+import { FiClock } from "react-icons/fi";
+import { getTasksList } from "./dashboardApi";
 
 const Dashboard = () => {
+  const [tasksLists, setTasksLists] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchTasksLists = async () => {
+      try {
+        const data = await getTasksList();
+        if (Array.isArray(data.tasksLists)) {
+          setTasksLists(data.tasksLists);
+        } else {
+          console.error("Unexpected format for tasksLists:", data.tasksLists);
+        }
+        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching tasks:', error);
+        setLoading(false);
+      }
+    };
+
+    fetchTasksLists();
+  }, []);
+
+  useRootClass("root-special-style");
+  const name = localStorage.getItem("name");
+
   return (
     <div className="dashboard">
-      <Sidebar />
+      <Sidebar taskLists={tasksLists} />
       <header className="header">
-        <h1>Bem vindo, Fulano</h1>
+        <h1>Bem vindo, {name}</h1>
       </header>
       <main className="content">
         <section className="recent-access">
-          <h2>Acessos recentes</h2>
-          <div className="boards-list">
-            <div className="board">
-              <div className="board-icon">✓</div>
-              <p>Projeto x</p>
-            </div>
-            <div className="board">
-              <div className="board-icon">✓</div>
-              <p>Board 3</p>
-            </div>
-            <div className="board">
-              <div className="board-icon">✓</div>
-              <p>Board 2</p>
-            </div>
-            <div className="board">
-              <div className="board-icon">✓</div>
-              <p>Board 4</p>
-            </div>
-            <div className="board">
-              <div className="board-icon">✓</div>
-              <p>Task List</p>
-            </div>
-          </div>
+          <h2 className="text-recent-access">
+            <FiClock style={{ marginRight: "3px" }} /> Últimas listas
+          </h2>
+          {loading ? (
+            <p>Carregando...</p>
+          ) : (
+            tasksLists.length > 0 ? (
+              tasksLists.map((lista) => (
+                <div key={lista.idTaskList} className="card">
+                  <div className="card-header">
+                    <img src="./check.png" alt="Icon" className="card-icon" />
+                  </div>
+                  <div className="card-content">
+                  <h3>{lista.taskListName}</h3>
+                    <p>{lista.taskListDescription}</p>
+                    {lista.tasks.length > 0 && (
+                      <ul>
+                        {lista.tasks.map((task, index) => (
+                          <li key={index}>{task}</li>
+                        ))}
+                      </ul>
+                    )}
+                  </div>
+                </div>
+              ))
+            ) : (
+              <p>Nenhuma task list disponível.</p>
+            )
+          )}
         </section>
       </main>
     </div>
--- src/views/src/components/dashboard/dashboardApi.js
+++ src/views/src/components/dashboard/dashboardApi.js
@@ -1,24 +1,22 @@
 
 
 
-export async function login(email, password) {
+export async function getTasksList() {
     try {
-        const response = await fetch('http://localhost:3001/user/login', {
-            method: 'POST',
+        const response = await fetch('http://localhost:3001/tasks-lists/all', {
+            method: 'GET',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({ email, password }),
+            
             credentials: 'include', // Ensure credentials are sent
         });
 
         if (response.ok) {
-        let answer = await response.json();
-            localStorage.setItem('name', answer?.user?.name);
             return await response.json();
         } else {
             const errorData = await response.json();
-            throw new Error(errorData.error || 'Erro ao fazer o login');
+            throw new Error(errorData.error || 'Erro ao buscar lista de tasks');
         }
         
     } catch (error) {
